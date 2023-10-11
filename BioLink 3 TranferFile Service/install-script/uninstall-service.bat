97e4375fa79360a156d916e77bab24fc9808f497
Jame Adisorn <62010897+adisorncjame@users.noreply.github.com>
2023-10-11 11:24:54 +0700

    Release 3.1
    Release 3.1 Standard Package

---
+++ BioLink 3 TranferFile Service/install-script/uninstall-service.bat
@@ -0,0 +1,9 @@
+@echo off
+cd C:\Windows\Microsoft.NET\Framework\v4.0.30319
+installutil.exe -u C:\ControlA\BL3TF.exe
+if ERRORLEVEL 1 goto error 
+exit
+:error
+color 0C
+echo There was a problem
+pause
